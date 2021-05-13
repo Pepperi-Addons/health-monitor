649f89edae07c6d7f5e598c89567f7c61e4c701a
Amir <amir.f@pepperi.com>
2021-05-13 14:58:09 +0300

    Added pepperu usage monitor code to health monitor addon.

--- server-side/installation.ts
+++ server-side/installation.ts
@@ -11,7 +11,63 @@ import { PapiClient, CodeJob, AddonDataScheme } from "@pepperi-addons/papi-sdk";
 import { Client, Request } from '@pepperi-addons/debug-server'
 import jwtDecode from "jwt-decode";
 
+export const PepperiUsageMonitorTable: AddonDataScheme = {
+    Name: "PepperiUsageMonitor",
+    Type: "data"
+}
+
 exports.install = async (client: Client, request: Request) => {
+    // Pepperi Usage Monitor
+    try {
+        const papiClient = new PapiClient({
+            baseURL: client.BaseURL,
+            token: client.OAuthAccessToken,
+            addonUUID: client.AddonUUID,
+            addonSecretKey: client.AddonSecretKey
+        });
+
+        // Install scheme for Pepperi Usage Monitor
+        try {
+            await papiClient.addons.data.schemes.post(PepperiUsageMonitorTable);
+            console.log('PepperiUsageMonitor Table installed successfully.');
+        }
+        catch (err) {
+            return {
+                success: false,
+                errorMessage: ('message' in err) ? err.message : 'Could not install HealthMonitorAddon. Create PepperiUsageMonitor table failed.',
+            }
+        }
+
+        // Install code job for Pepperi Usage Monitor
+        try {
+            const codeJob: CodeJob = await papiClient.codeJobs.upsert({
+                CodeJobName: "Pepperi Usage Monitor Addon Code Job",
+                Description: "Pepperi Usage Monitor",
+                Type: "AddonJob",
+                IsScheduled: true,
+                CronExpression: getCronExpression(),
+                AddonPath: "apiSuccessMonitor",
+                FunctionName: "run_collect_data",
+                AddonUUID: client.AddonUUID,
+                NumberOfTries: 30,
+            });
+            console.log('PepperiUsageMonitor code job installed successfully.');
+        }
+        catch (err)
+        {
+            return {
+                success: false,
+                errorMessage: ('message' in err) ? err.message : 'Could not install HealthMonitorAddon. Create PepperiUsageMonitor code job failed.',
+            }
+        }
+    }
+    catch (err) {
+        return {
+            success: false,
+            errorMessage: ('message' in err) ? err.message : 'Cannot install HealthMonitorAddon (PepperiUsageMonitor). Unknown Error Occured',
+        };
+    }
+
     try {
         let success = true;
         let errorMessage = '';
@@ -456,6 +512,47 @@ async function GetDistributor(papiClient){
         MachineAndPort: machineData.Machine + ":" + machineData.Port
     };
     return distributor;
+}
+
+function getCronExpression() {
+    let expressions = [
+        '0 19 * * FRI',
+        '0 20 * * FRI',
+        '0 21 * * FRI',
+        '0 22 * * FRI',
+        '0 23 * * FRI',
+        '0 0 * * SAT',
+        '0 1 * * SAT',
+        '0 2 * * SAT',
+        '0 3 * * SAT',
+        '0 4 * * SAT',
+        '0 5 * * SAT',
+        '0 6 * * SAT',
+        '0 7 * * SAT',
+        '0 8 * * SAT',
+        '0 9 * * SAT',
+        '0 10 * * SAT',
+        '0 11 * * SAT',
+        '0 12 * * SAT',
+        '0 13 * * SAT',
+        '0 14 * * SAT',
+        '0 15 * * SAT',
+        '0 16 * * SAT',
+        '0 17 * * SAT',
+        '0 18 * * SAT',
+        '0 19 * * SAT',
+        '0 20 * * SAT',
+        '0 21 * * SAT',
+        '0 22 * * SAT',
+        '0 23 * * SAT',
+        '0 0 * * SUN',
+        '0 1 * * SUN',
+        '0 2 * * SUN',
+        '0 3 * * SUN',
+        '0 4 * * SUN',        
+    ]
+    const index = Math.floor(Math.random() * expressions.length);
+    return expressions[index];
 }
 
 //#endregion
