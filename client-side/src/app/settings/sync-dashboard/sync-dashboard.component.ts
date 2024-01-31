941c07350ac5e4f79474f561a3eef2984c761490
roiaharon-pepperi <roi.a@pepperi.com>
2024-01-31 16:03:38 +0200

    fix tooltip positioning

--- client-side/src/app/settings/sync-dashboard/sync-dashboard.component.ts
+++ client-side/src/app/settings/sync-dashboard/sync-dashboard.component.ts
@@ -20,39 +20,39 @@ export class SyncDashboardComponent implements OnInit {
 
   isLoaded: boolean = false;
   syncEmpty: boolean = true;
-  
+
   tabID = 0;
 
   items: any[] = [];
 
-  @ViewChild ('hourlySync') canvasHourlySync: ElementRef;
-  @ViewChild ('dailySync') canvasDailySync: ElementRef;
-  @ViewChild ('weeklySync') canvasWeeklySync: ElementRef;
-  @ViewChild ('monthlySync') canvasMonthlySync: ElementRef;
+  @ViewChild('hourlySync') canvasHourlySync: ElementRef;
+  @ViewChild('dailySync') canvasDailySync: ElementRef;
+  @ViewChild('weeklySync') canvasWeeklySync: ElementRef;
+  @ViewChild('monthlySync') canvasMonthlySync: ElementRef;
 
   constructor(
     public addonService: AddonService
-    ) { 
-      Chart.register(...registerables);
-      Chart.defaults.maintainAspectRatio = false;
-    }
+  ) {
+    Chart.register(...registerables);
+    Chart.defaults.maintainAspectRatio = false;
+  }
 
   ngOnInit() {
     this.addonService.initHealthMonitorDashaboardData().then(result => {
       this.dashboardData = result;
-      this.dashboardData.LastSync.StatusName = result.LastSync.Status? 'Success' : 'Delayed';
-      this.dashboardData.LastSync.Color = result.LastSync.Status? 'inherit' : 'rgba(255, 89, 90, 1)'; 
+      this.dashboardData.LastSync.StatusName = result.LastSync.Status ? 'Success' : 'Delayed';
+      this.dashboardData.LastSync.Color = result.LastSync.Status ? 'inherit' : 'rgba(255, 89, 90, 1)';
     });
 
     this.addonService.initChartsData().then((result: any) => {
       this.syncData = result;
       this.syncEmpty = Object.keys(this.syncData).length === 0;
       this.isLoaded = true;
-      if(!this.syncEmpty) {
+      if (!this.syncEmpty) {
         this.loadData();
         this.uptimeValues = Object.values(this.syncData?.UptimeSync.data);
       }
-    });  
+    });
   }
 
   loadData() {
@@ -63,14 +63,14 @@ export class SyncDashboardComponent implements OnInit {
     }
 
     Object.entries(this.syncData).forEach(([key, value]: [string, any]) => {
-      if(key !== 'UptimeSync') {
-        if(key === 'HourlySyncs') {
+      if (key !== 'UptimeSync') {
+        if (key === 'HourlySyncs') {
           this.loadHourlySync();
         } else {
           this.ctxDailySync = this.canvasDailySync.nativeElement.getContext('2d');
 
-          const syncsData = (key === 'LastDaySyncs') ? 
-          value.data.map((item) => { return [item] }) : value.data[0].map((_, colIndex) => value.data.map(row => row[colIndex]));
+          const syncsData = (key === 'LastDaySyncs') ?
+            value.data.map((item) => { return [item] }) : value.data[0].map((_, colIndex) => value.data.map(row => row[colIndex]));
 
           this.loadAggregatedSyncStatus(this.syncData[key].dates, ctxTable[key], syncsData);
         }
@@ -81,95 +81,52 @@ export class SyncDashboardComponent implements OnInit {
   loadHourlySync() {
     this.ctxHourlySync = this.canvasHourlySync.nativeElement.getContext('2d');
     const data = this.syncData.HourlySyncs.data[0].map((_, colIndex) => this.syncData.HourlySyncs.data.map(row => row[colIndex]));
-    
+
     return new Chart(this.ctxHourlySync, {
       type: 'line',
       data: {
         labels: this.syncData.HourlySyncs.dates,
         datasets: [{
-            label: "Success",
-            data: data[2],
-            borderColor: 'rgba(144, 238, 144)', // green
-            tension: 0.1,
-            backgroundColor: 'rgb(144, 238, 144)',
-            pointBorderColor: 'rgb(144, 238, 144)',
-            pointHoverBackgroundColor: 'rgb(144, 238, 144)',
-            pointHoverBorderColor: 'rgb(144, 238, 144)',
-            
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            spanGaps: false
-          },
-          {
-            label: "Failed",
-            data:  data[1],
-            borderColor: 'rgb(255, 0, 0)', // red
-            tension: 0.1,
-            backgroundColor: 'rgb(255, 0, 0)',
-            pointBorderColor: 'rgb(255, 0, 0)',
-            pointHoverBackgroundColor: 'rgb(255, 0, 0)',
-            pointHoverBorderColor: 'rgb(255, 0, 0)',
-            
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            spanGaps: false
-          },
-          {
-            label: "Delayed",
-            data:  data[0],
-            borderColor: 'rgb(255, 165, 0)', // orange
-            tension: 0.1,
-            backgroundColor: 'rgb(255, 165, 0)',
-            pointBorderColor: 'rgb(255, 165, 0)',
-            pointHoverBackgroundColor: 'rgb(255, 165, 0)',
-            pointHoverBorderColor: 'rgb(255, 165, 0)',
-
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            spanGaps: false
-          }]
+          label: "Success",
+          data: data[2],
+          borderColor: 'rgba(144, 238, 144)', // green
+          backgroundColor: 'rgb(144, 238, 144)',
+        },
+        {
+          label: "Failed",
+          data: data[1],
+          borderColor: 'rgb(255, 0, 0)', // red
+          backgroundColor: 'rgb(255, 0, 0)',
+        },
+        {
+          label: "Delayed",
+          data: data[0],
+          borderColor: 'rgb(255, 165, 0)', // orange
+          backgroundColor: 'rgb(255, 165, 0)',
+        }]
       },
       options: {
-        interaction: {
-          mode: 'point',
-          axis: 'x',
-          intersect: false
+        animation: {
+          duration: 0 // general animation time
+        },
+        elements: {
+          line: {
+            tension: 0 // disables bezier curves
+          }
         },
         plugins: {
-          tooltip: {
-           enabled: true,
-           mode: 'nearest'
-         },          
           legend: {
             display: true,
-          }  
+          },
+          tooltip: {
+            enabled: true,
+            mode: 'x',
+            intersect: false,
+            position: 'nearest'
+          }
         }
       }
-      
+
     });
   }
 
@@ -198,7 +155,8 @@ export class SyncDashboardComponent implements OnInit {
           backgroundColor: 'rgb(255, 0, 0)',
           borderWidth: 1,
           barThickness: 30
-        }]},
+        }]
+      },
       options: {
         scales: {
           y: {
@@ -212,5 +170,5 @@ export class SyncDashboardComponent implements OnInit {
       },
     });
   }
-    
+
 }  
